1275cd08135d1bd07fce493cf47c6fc8ca6f92ab
lukman549 <asumanlukomwa003@gmail.com>
2025-12-17 17:08:13 +0300

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,7 @@
+module.exports = {
+  ownerNumber: "256700123456",
+  botName: "DARKCORE-XMD",
+  prefix: "!",
+  supportGroup: "https://chat.whatsapp.com/YOUR_GROUP_LINK",
+  adminUsers: ["2567xxxxxxx", "2567xxxxxxx"]
+};
